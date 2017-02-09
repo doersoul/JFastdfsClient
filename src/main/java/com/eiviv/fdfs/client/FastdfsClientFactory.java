8bf2dd6d9e742ae4135a9728261edec26413e668
doer <doersoul@126.com>
2017-02-09 13:52:34 +0800

    update

--- src/main/java/com/eiviv/fdfs/client/FastdfsClientFactory.java
+++ src/main/java/com/eiviv/fdfs/client/FastdfsClientFactory.java
@@ -9,43 +9,59 @@ import com.eiviv.fdfs.config.FastdfsClientConfig;
 
 public class FastdfsClientFactory {
 	
-	private final static String configFile = "fastdfs.properties";
+	private final static String DEFAULT_CONFIG_FILE = "fastdfs.properties";
 	private static volatile FastdfsClient fastdfsClient;
-	private static FastdfsClientConfig config = null;
-	
+
 	private FastdfsClientFactory() {
 	}
-	
+
 	/**
 	 * 获取 fastdfs client 客户端
-	 * 
-	 * @return FastdfsClient实例
-	 */
-	public static FastdfsClient getFastdfsClient() {
+	 *
+	 * @param config
+	 * @return
+     */
+	public static FastdfsClient getFastdfsClient(FastdfsClientConfig config) {
 		if (fastdfsClient == null) {
 			synchronized (FastdfsClient.class) {
 				if (fastdfsClient == null) {
-					config = new FastdfsClientConfig(configFile);
-					
 					int connectTimeout = config.getConnectTimeout();
 					int networkTimeout = config.getNetworkTimeout();
 					List<String> trackerAddrs = config.getTrackerAddrs();
-					
+
 					TrackerClientFactory tcf = new TrackerClientFactory(connectTimeout, networkTimeout);
 					StorageClientFactory scf = new StorageClientFactory(connectTimeout, networkTimeout);
-					
+
 					GenericKeyedObjectPoolConfig tcpc = config.getTrackerClientPoolConfig();
 					GenericKeyedObjectPoolConfig scpc = config.getStorageClientPoolConfig();
-					
+
 					GenericKeyedObjectPool<String, TrackerClient> tcp = new GenericKeyedObjectPool<String, TrackerClient>(tcf, tcpc);
 					GenericKeyedObjectPool<String, StorageClient> scp = new GenericKeyedObjectPool<String, StorageClient>(scf, scpc);
-					
+
 					fastdfsClient = new FastdfsClient(trackerAddrs, tcp, scp);
 				}
 			}
 		}
-		
+
 		return fastdfsClient;
 	}
-	
+
+	/**
+	 * 获取 fastdfs client 客户端
+	 *
+	 * @param configFile
+	 * @return
+     */
+	public static FastdfsClient getFastdfsClient(String configFile) {
+		return FastdfsClientFactory.getFastdfsClient(new FastdfsClientConfig(configFile));
+	}
+
+	/**
+	 * 获取 fastdfs client 客户端
+	 *
+	 * @return
+     */
+	public static FastdfsClient getFastdfsClient() {
+		return FastdfsClientFactory.getFastdfsClient(DEFAULT_CONFIG_FILE);
+	}
 }
